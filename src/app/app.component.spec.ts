cb31c97ffc8ae8f543ba84a091d7bba08a682059
alban-corno <alban.corno.pro@gmail.com>
2025-11-05 12:34:36 +0100

    new yaml

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,22 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { PokemonListeComponent } from './pokemon-liste/pokemon-liste.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        PokemonListeComponent,
+        HttpClientTestingModule,
+        RouterTestingModule
+      ]
     }).compileComponents();
   });
 
+
+  
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
