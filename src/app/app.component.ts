cb7ac7013e3384384b30675163c48f61485a2f77
alban-corno <alban.corno.pro@gmail.com>
2025-11-05 10:35:50 +0100

    Try to migrate, new navigation component and some css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PokeGuessComponent } from './poke-guess/poke-guess.component';
 import { PokemonListeComponent } from './pokemon-liste/pokemon-liste.component';
+import { AppNavigationComponent } from './app-navigation/app-navigation.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PokeGuessComponent, PokemonListeComponent],
+  imports: [RouterOutlet, PokeGuessComponent, PokemonListeComponent, AppNavigationComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { PokeGuessComponent } from './poke-guess/poke-guess.component';
+import { PokemonListeComponent } from './pokemon-liste/pokemon-liste.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'guess', pathMatch: 'full' },
+  { path: 'guess', component: PokeGuessComponent },
+  { path: 'liste', component: PokemonListeComponent },
+];
